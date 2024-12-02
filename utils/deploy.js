36eec18aa0cfb3098f8b48f3fcff9a663829b963
Sykander <26931543+Sykander@users.noreply.github.com>
2024-12-02 16:40:52 +0000

    Updated Deployment code to deploy snippets and run concurrently Added snippets for targetting

--- utils/deploy.js
+++ utils/deploy.js
@@ -1,45 +1,164 @@
-// @ts-check
-/** @typedef {import("./types").source_map} source_map */
+const fs = require("fs");
 
 const devSourceMap = require("./sourcemap-dev.json");
 const prodSourceMap = require("./sourcemap-prod.json");
-const { deployGvar } = require("./deploy-gvar");
-const { deployAliases } = require("./deploy-alias");
-const { readSyncEnv } = require("./read-file-sync-environment");
+const { getWorkshop } = require("./avrae/get-workshop");
+const { getAlias } = require("./avrae/get-alias");
+const { setEnvironmentId } = require("./set-environment-id");
+const { updateAlias } = require("./avrae/update-alias");
+const { getSnippet } = require("./avrae/get-snippet");
+const { updateSnippet } = require("./avrae/update-snippet");
+const { updateGvar } = require("./avrae/update-gvar");
+const { getGvar } = require("./avrae/get-gvar");
 
 const env = process.env.ENV || "Development";
+const sourceMaps = {
+  Development: devSourceMap,
+  Production: prodSourceMap,
+};
 
-/** @type {source_map} */
-// @ts-ignore
-const source_map = env === "Production" ? prodSourceMap : devSourceMap;
+const sourceMap = sourceMaps[env];
 
-const { workshop, aliases, gvars, snippets, environment } = source_map;
+if (!sourceMap) {
+  throw new Error(`No sourcemap found for env ${env}.`);
+}
 
-new Promise(async (success, reject) => {
-  try {
-    console.log(`--- Updating GVars ---`);
-    for (var { gvar_id, name, file } of gvars.values()) {
-      console.log(`Updating gvar "${name}"`);
-      const code = readSyncEnv(file, environment);
-      await deployGvar(gvar_id, name, code);
+function hydrateSourceMap(sourceMap, workshop) {
+  sourceMap.aliases.forEach((sourceMapAlias) => {
+    const workshopAlias = workshop.aliases.find(
+      ({ name: workshopAliasName }) =>
+        sourceMapAlias.name === workshopAliasName,
+    );
+
+    if (!workshopAlias) {
+      throw new Error(
+        `Alias ${sourceMapAlias.name} does not exist in the workshop.`,
+      );
     }
 
-    console.log(`\n--- Updating Aliases ---`);
-    for (var alias of aliases) {
-      console.log(`Updating alias category "${alias.group_name}"`);
-      await deployAliases(workshop.id, alias, environment);
+    sourceMapAlias.id = workshopAlias._id;
+
+    sourceMapAlias.sub_aliases.forEach((sourceMapSubAlias) => {
+      const workshopSubCommand = workshopAlias.subcommands.find(
+        ({ name: subCommandName }) => subCommandName === sourceMapSubAlias.name,
+      );
+
+      if (!workshopSubCommand) {
+        throw new Error(
+          `Alias ${sourceMapAlias.name} sub alias ${sourceMapSubAlias.name} does not exist in the workshop.`,
+        );
+      }
+
+      sourceMapSubAlias.id = workshopSubCommand._id;
+    });
+  });
+  sourceMap.snippets.forEach((sourceMapSnippet) => {
+    const workshopSnippet = workshop.snippets.find(
+      ({ name: workshopSnippetName }) =>
+        sourceMapSnippet.name === workshopSnippetName,
+    );
+
+    if (!workshopSnippet) {
+      throw new Error(
+        `Snippet ${sourceMapSnippet.name} does not exist in the workshop.`,
+      );
     }
 
-    success(true);
-  } catch (error) {
-    // Propagate specific error without crashing
-    reject(error);
-  }
-})
-  .then((success) => {
-    console.log("\nReached end without errors.");
-  })
-  .catch((failure) => {
-    console.error("\n--- ERROR ---");
-    console.error(failure);
+    sourceMapSnippet.id = workshopSnippet._id;
+  });
+
+  return { ...sourceMap };
+}
+
+async function deploy(sourceMap) {
+  const workshop = await getWorkshop(sourceMap.workshop.id);
+  const hyrdratedSourceMap = hydrateSourceMap(sourceMap, workshop);
+
+  const tasks = [
+    ...[
+      ...hyrdratedSourceMap.aliases,
+      ...hyrdratedSourceMap.aliases.flatMap((alias) => alias.sub_aliases),
+    ].map(async (sourceMapAlias) => {
+      const aliasVersions = await getAlias(sourceMapAlias.id);
+      const aliasCurrentVersion =
+        aliasVersions.find(({ is_current }) => is_current) ||
+        aliasVersions[0] ||
+        {};
+      const highestVersion = Math.max(
+        ...aliasVersions.map(({ version }) => version),
+      );
+
+      const newContents = setEnvironmentId(
+        fs.readFileSync(sourceMapAlias.file).toString(),
+        sourceMap.workshop.environment,
+      );
+
+      if (aliasCurrentVersion.content === newContents) {
+        return;
+      }
+
+      const newVersion = {
+        ...aliasCurrentVersion,
+        is_current: true,
+        content: newContents,
+        version: highestVersion + 1,
+      };
+
+      return updateAlias(sourceMapAlias.id, newVersion);
+    }),
+    ...hyrdratedSourceMap.snippets.map(async (sourceMapSnippet) => {
+      const snippetVersions = await getSnippet(sourceMapSnippet.id);
+      const snippetCurrentVersion =
+        snippetVersions.find(({ is_current }) => is_current) ||
+        snippetVersions[0] ||
+        {};
+      const highestVersion = Math.max(
+        ...snippetVersions.map(({ version }) => version),
+      );
+
+      const newContents = setEnvironmentId(
+        fs.readFileSync(sourceMapSnippet.file).toString(),
+        sourceMap.workshop.environment,
+      );
+      if (snippetCurrentVersion.content === newContents) {
+        return;
+      }
+
+      const newVersion = {
+        ...snippetCurrentVersion,
+        is_current: true,
+        content: newContents,
+        version: highestVersion + 1,
+      };
+
+      return updateSnippet(sourceMapSnippet.id, newVersion);
+    }),
+    ...hyrdratedSourceMap.gvars.map(async (sourceMapGvar) => {
+      const currentGvar = await getGvar(sourceMapGvar.id);
+
+      const newContents = setEnvironmentId(
+        fs.readFileSync(sourceMapGvar.file).toString(),
+        sourceMap.workshop.environment,
+      );
+
+      if (currentGvar.value === newContents) {
+        return;
+      }
+
+      return updateGvar(sourceMapGvar.id, {
+        ...currentGvar,
+        value: newContents,
+      });
+    }),
+  ];
+
+  return Promise.all(tasks);
+}
+
+console.log("Starting Deployment");
+deploy(sourceMap)
+  .then(() => console.log("Deployment Sucessful"))
+  .catch((e) => {
+    console.error(e);
+    console.log("Deployment Failed");
   });
